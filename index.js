35196bb7f0f6e24b7dee65029f1fc610afb3486f
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-11 10:43:11 +0500

    ...

---
+++ index.js
@@ -0,0 +1,11 @@
+function son(a) {
+  for (let a = 1; a <= n; i++) {
+    let sum = 0;
+  }
+  return (a = a + n);
+}
+let n = +prompt("sonni kiriting");
+console.log(son(a, n));
+
+// Funksiyaga oid masalalar.
+//1-masala
